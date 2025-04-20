96b5986665375c45cb3f0e992c1612b09698efd5
han <dmitrijsuhanov713@gmail.com>
2025-04-20 19:33:12 +0300

    pieces created

---
+++ Core/Pieces/PieceColor.cs
@@ -0,0 +1,7 @@
+namespace Core.Pieces;
+
+public enum PieceColor
+{
+    White,
+    Black
+}
---
+++ Core/Pieces/PieceType.cs
@@ -0,0 +1,11 @@
+namespace Core.Pieces;
+
+public enum PieceType
+{
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
+}
